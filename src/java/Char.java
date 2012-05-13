b81e6a566451202451348f6ec8d4426f89dc6a10
Romain Pelisse <belaran@gmail.com>
2012-05-13 20:56:44 +0200

    Add Char example source code

---
+++ src/java/Char.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Probably one of the most powerfull piece of program I ever wrote... No, really !
+ *
+ * @author Romain PELISSE
+ *
+ */
+public class Char {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char a = 'a';
+		char b = 'b';
+        // Once this programming is running, modify to disply your firstname, instead of "ab"
+        System.out.println("Result:" + a + b );
+	}
+}
