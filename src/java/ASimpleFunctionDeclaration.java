29c961805df2547f0c32fc57fc5c54361a538ae4
Romain Pelisse <belaran@gmail.com>
2012-05-27 22:56:16 +0200

    Add picture for simple function

---
+++ src/java/ASimpleFunctionDeclaration.java
@@ -0,0 +1,18 @@
+
+public class ASimpleFunctionDeclaration {
+
+	/**
+	 * Adds the two integer values provided and return the 
+	 * results. 
+	 * 
+	 * @param firstArg, an integer value
+	 * @param secondArg, an integer value
+	 * @return the sum of both parameter
+	 */
+	public int add(int firstArg, int secondArg) {			
+		return firstArg + secondArg;
+	}
+}
+
+
+
