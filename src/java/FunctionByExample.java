b649184891971e62458f5d52d83dd9fd7864fd46
Romain Pelisse <belaran@gmail.com>
2012-06-01 16:02:28 +0200

    FunctionByExample correction

---
+++ src/java/FunctionByExample.java
@@ -0,0 +1,23 @@
+
+public class FunctionByExample {
+
+	public static int add(int a, int b) {
+		return (a + b);		
+	}
+	
+	public static void main(String[] args) {
+		int sum = FunctionByExample.add(21, 3);
+		System.out.println(sum);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
